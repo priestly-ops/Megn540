be9aaac563cd41ff9406790647786aa810e295a4
Keenan D. Buckley <keenandbuckley@protonmail.com>
2024-02-01 10:41:09 -0700

    Implement four math operations

--- c_lib/Lab1_Tasks.c
+++ c_lib/Lab1_Tasks.c
@@ -9,3 +9,27 @@ void Multiply_And_Send( float value_left, float value_right )
     // send response right here if appropriate.
     USB_Send_Msg( "cf", '*', &ret_val, sizeof( ret_val ) );
 }
+
+void Divide_And_Send( float value_left, float value_right )
+{
+    float ret_val = value_left / value_right;
+
+    // send response right here if appropriate.
+    USB_Send_Msg( "cf", '/', &ret_val, sizeof( ret_val ) );
+}
+
+void Add_And_Send( float value_left, float value_right )
+{
+    float ret_val = value_left + value_right;
+
+    // send response right here if appropriate.
+    USB_Send_Msg( "cf", '+', &ret_val, sizeof( ret_val ) );
+}
+
+void Subtract_And_Send( float value_left, float value_right )
+{
+    float ret_val = value_left - value_right;
+
+    // send response right here if appropriate.
+    USB_Send_Msg( "cf", '-', &ret_val, sizeof( ret_val ) );
+}
